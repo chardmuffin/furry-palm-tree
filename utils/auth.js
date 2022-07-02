4aa900e033573134d74cff1be5b13d1231d99705
Richard Huffman <richardhuffman96@gmail.com>
2022-07-02 18:27:32 -0400

    added withAuth, date_format helper, and user model

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+const withAuth = (res, req, next) => {
+    if (!req.session.user_Id) {
+        res.redirect('/login');
+    }
+    else {
+        next();
+    }
+};
+
+module.exports = withAuth;
